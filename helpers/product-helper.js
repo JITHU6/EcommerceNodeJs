c2404d52db434d3dd3f068270d680afb827d3459
jithu6 <jithu.benny6@gmail.com>
2020-11-11 13:07:33 +0530

    login completed

--- helpers/product-helper.js
+++ helpers/product-helper.js
@@ -1,12 +1,19 @@
 var db=require('../config/connection')
-
+var collection = require('../config/collections')
 module.exports = {
 
 
     addProduct:(product,callback)=>{
-        db.get().collection('products').insertOne(product).then((data)=>{
+        db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then((data)=>{
             console.log(data);
             callback(data.ops[0]._id); 
+        })
+    },
+    getAllProducts:()=>{
+        
+        return new Promise(async(resolve,reject)=>{
+            let product = await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(product)
         })
     }
 }
